6e825e9911f3a1682e611bea3b19e9a41707a99a
hdcunnendep <116704655+hdcunnendep@users.noreply.github.com>
2024-06-17 16:34:28 +0700

    Add files via upload

---
+++ maru.lua
@@ -0,0 +1,3 @@
+getgenv().Key = "MARU-EHMDK-LLMO5-79LN-0TU2U-TNLB"
+getgenv().id = "827138555041546270"
+loadstring(game:HttpGet("https://raw.githubusercontent.com/xshiba/MaruBitkub/main/Mobile.lua"))()
